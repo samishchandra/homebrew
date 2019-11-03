514876a1a0de627806c6a1dcbd18463a05244fc6
Samish Chandra Kolli <samishchandra@gmail.com>
2019-11-03 12:45:35 -0800

    Added Samish Uncrustify formula

---
+++ Formula/samish_uncrustify.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SamishUncrustify < Formula
+  desc "Samish's version of Uncrustify"
+  homepage "https://github.com/samishchandra/uncrustify"
+  url "https://github.com/uncrustify/uncrustify/archive/uncrustify-0.69.0.tar.gz"
+  sha256 ""
+  head "https://github.com/samishchandra/uncrustify.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+    doc.install (buildpath/"documentation").children
+  end
+
+  test do
+    (testpath/"t.c").write <<~EOS
+      #include <stdio.h>
+      int main(void) {return 0;}
+    EOS
+    expected = <<~EOS
+      #include <stdio.h>
+      int main(void) {
+      \treturn 0;
+      }
+    EOS
+
+    system "#{bin}/uncrustify", "-c", "#{doc}/htdocs/default.cfg", "t.c"
+    assert_equal expected, File.read("#{testpath}/t.c.uncrustify")
+  end
+end
